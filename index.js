fa9ba0bd69d4edec97cb895cf30a4b74b4b05638
Peter Ajala <ajalapeter1@gmail.com>
2023-04-01 16:42:13 -0700

    worked on JS functionality, html and styling

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-var [miliseconds, seconds, minutes, hours] = [0, 0, 0, 0]
-var timeDisplay = document.getElementById('timeDisplay');
-var timer = null;
+var [seconds, minutes, hours] = [0, 0, 0]
+let timeDisplay = document.getElementById('timeDisplay');
+let timer = null;
 
-function watchCount(){
-    miliseconds++;
-    if(miliseconds == 100){
-        miliseconds = 0;
-        seconds++;
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-            if(minutes == 60){
-                minutes = 0;
-                hours++;
-            }
+function watchCounter(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++;
         }
     }
 
-    let hr = hours
-        if(hours < 10) {hr = "0" + hours} else{hours}
-    let min = minutes
-        if (minutes < 10) {min = "0" + minutes} else{minutes}
+    let hr = hours 
+    if(hours < 10) {hr = "0" + hours} else{hours};
+    let min = minutes 
+    if(minutes < 10) {min = "0" + minutes} else{minutes};
     let sec = seconds < 10 ? "0" + seconds : seconds;
-    let milisec = miliseconds < 10 ? "0" + miliseconds : miliseconds;
 
-    document.getElementById('timeDisplay').innerHTML = hr +"h:"+ min +"m:"+ sec + "s:" + milisec;
+    document.getElementById('timeDisplay').innerHTML = hr +"h:"+ min +"m:"+ sec +"s";
 }
 
 const start = () =>{
     if(timer!== null){
         clearInterval(timer);
     }
-    timer = setInterval(watchCount, 10);
+    timer = setInterval(watchCounter, 1000);
 }
 
 const  stop = () =>{
@@ -40,8 +35,8 @@ const  stop = () =>{
 
 const reset = () =>{
     clearInterval(timer);
-    [miliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-    timeDisplay.innerHTML = "00h:00m:00s:00";
+    [seconds, minutes, hours] = [0, 0, 0];
+    document.getElementById('timeDisplay').innerHTML = "00h:00m:00s";
 }
 
    
@@ -52,55 +47,70 @@ const reset = () =>{
 
 
 
-//     var timer1,timer2,timer3,timer4 = null;
+// var timer1,timer2,timer3,timer4 = null;
     
-// //    function that will be excuted on click of the start button
-//     function start(){
-//         timer1 = setInterval(ms, 1);
-//         timer2 = setInterval(sec, 1000);
-//         timer3 = setInterval(min, 60000);
-//         timer4 = setInterval(hr, 3600000);
-        
+// // //    function that will be excuted on click of the start button
+// function start(){
+//     if(timer1!== null){
+//         clearInterval(timer1);
 //     }
+//     timer1 = setInterval(ms, 1);
 
-//     function ms(){
-//         let miliSeconds = Number(document.getElementById('milisecs').innerHTML);
-//         miliSeconds ++;
-//         if (miliSeconds == 100){
-//             miliSeconds = 0;
-//         }
-//         if(miliSeconds < 10){
-//             miliSeconds = "0" + miliSeconds;
+//     if(timer2!== null){
+//     clearInterval(timer2);
+//     }
+//     timer2 = setInterval(sec, 1000);
+
+//     if(timer3!== null){
+//         clearInterval(timer3);
+//     }
+//     timer3 = setInterval(min, 60000);
+
+//     if(timer4!== null){
+//         clearInterval(timer4);
+//     }
+//     timer4 = setInterval(hr, 3600000);
+        
+// }
+
+// function ms(){
+//     let miliSeconds = Number(document.getElementById('milisecs').innerHTML);
+//     miliSeconds ++;
+//     if (miliSeconds == 100){
+//          miliSeconds = 0;
+//     }
+//     if(miliSeconds < 10){
+//         miliSeconds = "0" + miliSeconds;
 //         }
         
 //         document.getElementById('milisecs').innerHTML = miliSeconds;
         
-//     }
+//      }
 
-//     function sec(){
-//         let secs = Number(document.getElementById('seconds').innerHTML);
-//         secs ++;
-//         if(secs == 60){
-//             secs = 0;
-//         }
-//         if(secs < 10){
+//      function sec(){
+//          let secs = Number(document.getElementById('seconds').innerHTML);
+//          secs ++;
+//          if(secs == 60){
+//              secs = 0;
+//          }
+//          if(secs < 10){
 //             secs = "0" + secs;
 //         }
 //         document.getElementById('seconds').innerHTML = secs;  
-//     }
-//     function min(){
-//         let mins = Number(document.getElementById('minutes').innerHTML);
-//         mins ++;
-//         if(mins == 60){
-//             mins = 0;
-//         }
+//      }
+//      function min(){
+//          let mins = Number(document.getElementById('minutes').innerHTML);
+//          mins ++;
+//          if(mins == 60){
+//              mins = 0;
+//          }
 //         if(mins < 10){
 //             mins = "0" + mins;
-//         }
-//         document.getElementById('minutes').innerHTML = mins; 
-//     }
-//     function hr(){
-//         let hrs = Number(document.getElementById('hours').innerHTML);
+//                  }
+//          document.getElementById('minutes').innerHTML = mins; 
+//      }
+//      function hr(){
+//          let hrs = Number(document.getElementById('hours').innerHTML);
 //         hrs ++;
 //         if(hrs == 60){
 //             hrs = 0;
@@ -109,31 +119,27 @@ const reset = () =>{
 //             hrs = "0" + hrs;
 //         }
 //         document.getElementById('hours').innerHTML = hrs; 
-//     }
-// // function that will be executed on click of the stop button
+//      }
+// // // function that will be executed on click of the stop button
 //     function stop(){
 //         clearInterval(timer1)
 //         clearInterval(timer2)
 //         clearInterval(timer3)
-//         clearInterval(timer4);
-       
-        
-//     }
-// // function that will be executed on click of the reset button
-//     function reset(){
+//          clearInterval(timer4);
+//         }
+
+// // // function that will be executed on click of the reset button
+//      function reset(){
 //         document.getElementById('milisecs').innerHTML = "00";
 //         document.getElementById('seconds').innerHTML = "00";
 //         document.getElementById('minutes').innerHTML = "00";
 //         document.getElementById('hours').innerHTML = "00";
-//         clearInterval(timer1)
-//         clearInterval(timer2)
-//         clearInterval(timer3)
-//         clearInterval(timer4)
+//          clearInterval(timer1)
+//          clearInterval(timer2)
+//          clearInterval(timer3)
+//          clearInterval(timer4)
 //     }
 
 
 
-    // let hr = hours < 10 ? "0" + hours : hours;
-    // let min = minutes < 10 ? "0" + minutes : minutes;
-    // let sec = seconds < 10 ? "0" + seconds : seconds;
-    // let milisec = miliseconds < 10 ? "0" + miliseconds : miliseconds;
+    // 
